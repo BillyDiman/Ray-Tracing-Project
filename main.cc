edc90edca26a73d8343e5673c7f929abb39ef16b
Billy Diman <134522887+BillyDiman@users.noreply.github.com>
2026-04-21 09:18:49 -0400

    Mini main

---
+++ main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+
+
+    //Image properties
+    int imageWidth = 256;
+    int imageHeight = 256;
+
+
+    //Render
+
+    //Print PPM header
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for(int j = 0; j < imageHeight; j++){
+        for(int i = 0; i < imageWidth; i++){
+            auto r = double(i) / (imageWidth-1);
+            auto g = double(j) / (imageHeight-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
